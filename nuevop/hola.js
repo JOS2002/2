b9854bd0ec1c1f1adeedaace4c2bb3f573894eb3
JOS2002 <64385264+JOS2002@users.noreply.github.com>
2020-04-29 17:47:37 -0500

    Add files via upload

---
+++ nuevop/hola.js
@@ -0,0 +1,17 @@
+var usuario = document.getElementById('usuario');
+var contraseña = document.getElementById('contraseña');
+var boton = document.getElementById('boton');
+var cuadro = document.getElementById('cuadro');
+
+boton.onclick=function(){
+   if( usuario. value === null || usuario.value === '') {
+   alert("coloque su usuario");}
+   if( contraseña. value === null || contraseña.value === ''){;
+      alert("coloque su contraseña");}
+
+      else {(alert("fg"))||(cuadro.style.display="none");}
+
+
+
+
+}
